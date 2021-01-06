ff505748aaffcf5497beb8d9bee6511f016cc55e
andrejstrashun <andrejstrashun@gmail.com>
2021-01-06 21:27:20 +0200

    less

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,23 @@ var webpack = require('webpack');
 module.exports = {
 
     entry: './src/index.js',
+    mode: 'development',
     output: {
         path: __dirname + '/public',
         filename: 'bundle.js'
-    }
+    },
+    module: {
+        rules: [
+            
+                {
+                    test: /\.css$/,
+                    use: 'css-loader',
+                },
+                {
+                    test: /\.less$/,
+                    use: 'less-loader',
+                },
+            
+        ],
+    },
 };
